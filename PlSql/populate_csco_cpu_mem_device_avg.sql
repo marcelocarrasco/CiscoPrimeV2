bead4717bb1a4252c322eb786672eaeab1c5d13c
Marcelo Carrasco <marceloacarrasco@gmail.com>
2016-09-10 10:40:41 -0300

    cmm

--- PlSql/populate_csco_cpu_mem_device_avg.sql
+++ PlSql/populate_csco_cpu_mem_device_avg.sql
@@ -3,32 +3,57 @@
 --
 INSERT INTO CSCO_CPU_MEM_DEVICE_AVG_BH (FECHA,NODE,CPUUTILMAX5MINDEVICEAVG,CPUUTILAVG5MINDEVICEAVG,CPUUTILMAX1MINDEVICEAVG,
                                       CPUUTILAVG1MINDEVICEAVG,USEDBYTESDEVICEAVG,FREEBYTESDEVICEAVG,AVGUTILDEVICEAVG,MAXUTILDEVICEAVG)
-WITH  SENDBYTES AS  (SELECT /*+ MATERIALIZE */
-                            FECHA,
-                            NODE,
-                            MAX(SENDBYTES) AS SENDBYTES_MAX
-                    FROM CSCO_INTERFACE_HOUR
-                    WHERE TO_CHAR(FECHA,'DD.MM.YYYY') = '28.07.2016'
-                    GROUP BY FECHA,NODE
-                    ORDER BY SENDBYTES_MAX DESC),
-      RECEIVEBYTES AS (SELECT /*+ MATERIALIZE */
-                              FECHA,
-                              NODE,
-                              MAX(RECEIVEBYTES) AS RECEIVEBYTES_MAX
-                      FROM CSCO_INTERFACE_HOUR
-                      WHERE TO_CHAR(FECHA,'DD.MM.YYYY') = '28.07.2016'
-                      GROUP BY FECHA,NODE
-                      ORDER BY  RECEIVEBYTES_MAX DESC),
-      MAX_SENDBYTES AS  (SELECT FECHA,
+                                      
+
+WITH  MAX_SENDBYTES AS  (SELECT NODE||','||FECHA|| NODE_FECHA,
                                 NODE,
-                                SENDBYTES_MAX
-                        FROM  SENDBYTES
-                        WHERE ROWNUM = 1),
-      MAX_RECEIVEBYTES  AS  (SELECT FECHA,
-                                    NODE,
-                                    RECEIVEBYTES_MAX
-                            FROM  RECEIVEBYTES
-                            WHERE ROWNUM = 1)
+                                FECHA,
+                                SENDBYTES
+                        FROM (SELECT  FECHA,
+                                      NODE,
+                                      SENDBYTES,
+                                      ROW_NUMBER() OVER( PARTITION BY NODE,TRUNC(FECHA) ORDER BY SENDBYTES DESC) RN
+                              FROM (SELECT  /*+ MATERIALIZE */
+                                            FECHA,
+                                            NODE,
+                                            SENDBYTES,
+                                            ROW_NUMBER() OVER( PARTITION BY NODE,FECHA ORDER BY SENDBYTES DESC) RNK 
+                                    FROM CSCO_INTERFACE_HOUR
+                                    WHERE TO_CHAR(FECHA,'DD.MM.YYYY') = '23.08.2016'
+                                  )
+                              WHERE RNK = 1
+                              ORDER BY SENDBYTES DESC)
+                        WHERE RN = 1),
+      MAX_RECEIVEBYTES AS (SELECT NODE||','||FECHA|| NODE_FECHA,
+                                  NODE,
+                                  FECHA,
+                                  RECEIVEBYTES
+                          FROM (SELECT  FECHA,
+                                        NODE,
+                                        RECEIVEBYTES,
+                                        ROW_NUMBER() OVER( PARTITION BY NODE,TRUNC(FECHA) ORDER BY RECEIVEBYTES DESC) RN
+                                FROM (SELECT  /*+ MATERIALIZE */
+                                              FECHA,
+                                              NODE,
+                                              RECEIVEBYTES,
+                                              ROW_NUMBER() OVER( PARTITION BY NODE,FECHA ORDER BY RECEIVEBYTES DESC) RNK 
+                                      FROM CSCO_INTERFACE_HOUR
+                                      WHERE TO_CHAR(FECHA,'DD.MM.YYYY') = '23.08.2016'
+                                    )
+                                WHERE RNK = 1
+                                ORDER BY RECEIVEBYTES DESC)
+                          WHERE RN = 1),
+      PARES_NODE_FECHA  AS  (SELECT  CASE
+                                      WHEN  MAX_SENDBYTES.SENDBYTES > MAX_RECEIVEBYTES.RECEIVEBYTES  THEN  MAX_SENDBYTES.NODE_FECHA
+                                      WHEN  MAX_SENDBYTES.SENDBYTES < MAX_RECEIVEBYTES.RECEIVEBYTES  THEN  MAX_RECEIVEBYTES.NODE_FECHA
+                                      ELSE  MAX_SENDBYTES.NODE_FECHA
+                                    END NODE_FECHA
+                            FROM  MAX_SENDBYTES,
+                                  MAX_RECEIVEBYTES
+                            WHERE MAX_SENDBYTES.NODE = MAX_RECEIVEBYTES.NODE
+                            AND   MAX_SENDBYTES.FECHA = MAX_RECEIVEBYTES.FECHA)
+SELECT NODE_FECHA FROM PARES_NODE_FECHA
+
 SELECT  FECHA,
         CCDVAH.NODE NODE,
         CPUUTILMAX5MINDEVICEAVG,
@@ -40,21 +65,61 @@ SELECT  FECHA,
         AVGUTILDEVICEAVG,
         MAXUTILDEVICEAVG
 FROM  CSCO_CPU_MEM_DEVICE_AVG_HOUR CCDVAH
-WHERE CCDVAH.NODE = (SELECT  CASE
-                              WHEN  MAX_SENDBYTES.SENDBYTES_MAX > MAX_RECEIVEBYTES.RECEIVEBYTES_MAX  THEN  MAX_SENDBYTES.NODE
-                              WHEN  MAX_SENDBYTES.SENDBYTES_MAX < MAX_RECEIVEBYTES.RECEIVEBYTES_MAX  THEN  MAX_RECEIVEBYTES.NODE
-                              ELSE  MAX_SENDBYTES.NODE
-                            END NODE
-                    FROM  MAX_SENDBYTES,
-                          MAX_RECEIVEBYTES)
-AND CCDVAH.FECHA = (SELECT  CASE
-                              WHEN  MAX_SENDBYTES.SENDBYTES_MAX > MAX_RECEIVEBYTES.RECEIVEBYTES_MAX  THEN  MAX_SENDBYTES.FECHA
-                              WHEN  MAX_SENDBYTES.SENDBYTES_MAX < MAX_RECEIVEBYTES.RECEIVEBYTES_MAX  THEN  MAX_RECEIVEBYTES.FECHA
-                              ELSE  MAX_SENDBYTES.FECHA
-                            END FECHA
-                    FROM  MAX_SENDBYTES,
-                          MAX_RECEIVEBYTES);
+WHERE (CCDVAH.NODE,CCDVAH.FECHA) = (('C1900-PD-02','23.08.2016 22:00:00'));,
+('CF223-BR-03','23.08.2016 22:00:00'),
+('CF223-BR-01','23.08.2016 22:00:00'),
+('CO008-P-01','23.08.2016 20:00:00'),
+('C1900-BR-03','23.08.2016 22:00:00'),
+('ngry01rt33','23.08.2016 21:00:00'));	
 
+--
+-- OPCION 2
+--
+-- RECEIVEBYTES --
+--
+SELECT  '('''||NODE||''','''||FECHA||''')' NODE_FECHA,
+        RECEIVEBYTES
+FROM (SELECT  FECHA,
+              NODE,
+              RECEIVEBYTES,
+              ROW_NUMBER() OVER( PARTITION BY NODE,TRUNC(FECHA) ORDER BY RECEIVEBYTES DESC) RN
+      FROM (
+          SELECT /*+ MATERIALIZE */
+                  FECHA,
+                  NODE,
+                  RECEIVEBYTES,
+                  ROW_NUMBER() OVER( PARTITION BY NODE,FECHA ORDER BY RECEIVEBYTES DESC) RNK 
+                  
+          FROM CSCO_INTERFACE_HOUR
+          WHERE TO_CHAR(FECHA,'DD.MM.YYYY') = '23.08.2016'
+          )
+      WHERE RNK = 1
+      ORDER BY 3 DESC)
+WHERE RN = 1;
+--
+-- SENDBYTES --
+--
+SELECT  '('''||NODE||''','''||FECHA||''')' NODE_FECHA,
+        SENDBYTES
+FROM (SELECT  FECHA,
+              NODE,
+              SENDBYTES,
+              ROW_NUMBER() OVER( PARTITION BY NODE,TRUNC(FECHA) ORDER BY SENDBYTES DESC) RN
+      FROM (
+          SELECT /*+ MATERIALIZE */
+                  FECHA,
+                  NODE,
+                  SENDBYTES,
+                  ROW_NUMBER() OVER( PARTITION BY NODE,FECHA ORDER BY SENDBYTES DESC) RNK 
+                  
+          FROM CSCO_INTERFACE_HOUR
+          WHERE TO_CHAR(FECHA,'DD.MM.YYYY') = '23.08.2016'
+          )
+      WHERE RNK = 1
+      ORDER BY 3 DESC)
+WHERE RN = 1;
+--
+--
 
 
 -- HOUR
